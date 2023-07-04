486eb48baeac9a06cb98451a4b50eb6b6b0e1b35
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-04 17:51:49 +0600

    env settings is done

---
+++ android/app/src/main/kotlin/com/example/rest_api_rabbil_hasan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rest_api_rabbil_hasan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Rest API Rabbil Hasan",
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("REST API"),
+      ),
+    );
+  }
+}
+
+
